b2ab8e27f67c953304fb96523d9f96424ffe8deb
Sean OReilly <soreilly424@gmail.com>
2021-11-08 14:12:21 -0500

    added detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,127 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Detail() {
-  return <div>Detail Page</div>
+  return (
+    <Container>
+      <Background>
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/9D8AEB7DE234898392BFD20E7D9B112B841E920AF9A3F54CCFB966722AFF3461/scale?width=1920&aspectRatio=1.78&format=jpeg"
+          alt=""
+        />
+      </Background>
+
+      <ImageTitle>
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/EF737B93E2F2ABE27C74CBBEB322F18A421E7986129E9989587CEF2295B0447F/scale?width=1344&aspectRatio=1.78&format=png"
+          alt=""
+        />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>Play</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="" />
+        </GroupWatchButton>
+      </Controls>
+    </Container>
+  )
 }
 
 export default Detail
+
+const Container = styled.div`
+  min-height: calc(100vh -70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  padding: 0px 24px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  margin-right: 24.2px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+  text-transform: uppercase;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`
+
+const AddButton = styled.button`
+  border-radius: 50%;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.6);
+  margin-right: 16px;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`
+
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgb(0, 0, 0);
+`
